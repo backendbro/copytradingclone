737402d1e7b0c781239807610d4ffbefef3a0dc9
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-10-03 03:02:16 +0100

    aXXOUNT

---
+++ src/models/WithdrawalModel.js
@@ -0,0 +1,7 @@
+const {model, Schema} = require('mongoose')
+
+const WithDrawalSchema = ({
+
+})
+
+module.exports = model('WithDraw', WithDrawalSchema)
--- src/service/WithdrawalService.js
+++ src/service/WithdrawalService.js
@@ -1,8 +1,11 @@
 class WithDrawalService {
     async bank(req,res){
         const user = await User.findById(req.user.id)
+        if(!user){
+            return res.status(404).json({nessage: "USER DOES"})
+        }
         
-    }
+    }   
 }
 
 module.exports = new WithDrawalService()
