66ab7366e413637a3b0796b545283ba9a6fa1220
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-10-07 17:36:24 +0100

    Added more cors details

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const app = express()
 const connectDB = require('./database/database')
 const notFound = require('./middlewares/notFound')
 
-//app.use(cors());
 const corsOptions ={
-    origin:'*', 
-    credentials:true,           
-    optionSuccessStatus:200,
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
 }
+app.use(cors(corsOptions));
 
-app.use(cors(corsOptions)) 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
