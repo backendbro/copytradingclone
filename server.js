4c9ab63938bfbf2861f0847415b6b0161a1d7596
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-10-07 18:06:11 +0100

    Removed all bogus cors options

--- server.js
+++ server.js
@@ -6,17 +6,7 @@ const app = express()
 const connectDB = require('./database/database')
 const notFound = require('./middlewares/notFound')
 
-// const corsOptions ={
-//     origin:'http://localhost:3000', 
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors())
   
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
@@ -28,7 +18,7 @@ connectDB()
 
 
 app.get('/', (req,res) => {
-    res.send('Hello world')
+    res.send('Hello world 2')
 })
 
 
