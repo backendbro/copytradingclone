7b9f054dfb10a6037b5603beafc5197cefa79597
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-10-07 14:34:16 +0100

    Changed the position of cors()

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express()
 const connectDB = require('./database/database')
 const notFound = require('./middlewares/notFound')
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors());
+
 
 dotenv.config()
 
