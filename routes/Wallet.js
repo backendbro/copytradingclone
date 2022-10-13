7418e45d4f960808ccc9eeca01fa9edc27a699a4
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-10-13 13:41:47 +0100

    A

--- routes/Wallet.js
+++ routes/Wallet.js
@@ -4,7 +4,7 @@ const {protect, auth} = require('../middlewares/protect-route')
 const Wallet = require('../models/Wallet')
 router.use(protect)
 
-router.get('/', auth("user"), WalletService.getWallet)
+router.get('/', auth("user", "admin"), WalletService.getWallet)
 router.get('/:id', auth('admin'), WalletService.getSingleWallet)
 router.put('/:id', auth('admin'), WalletService.updateWallet)
 router.post('/', auth("admin"), WalletService.addWallet)
