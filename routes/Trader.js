18e0629b64ef269cabcdbbf9bf5decfb24a7d89f
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-10-15 23:09:48 +0100

    Editing the roles: Trader

--- routes/Trader.js
+++ routes/Trader.js
@@ -3,14 +3,14 @@ const TraderService = require('../service/Trader')
 const {protect, auth} = require('../middlewares/protect-route')
 const upload = require('../config/multer')
 
-router.use(protect, auth('user', 'admin'))
+router.use(protect)
 
-router.get('/:id', TraderService.getTrader)
-router.get('/', TraderService.getTraders)
-router.post('/', TraderService.createTrader)
-router.get('/search/:searchString', TraderService.searchTrader)
-router.put('/:id', upload.single("traderPhoto"), TraderService.updateTrader)
-router.get('/copiers', TraderService.getCopiers)
-router.get('/copiers/:id', TraderService.declineCopiers)
+router.get('/:id', auth('admin'), TraderService.getTrader)
+router.get('/', auth('user', 'admin'), TraderService.getTraders)
+router.post('/', auth('admin'), TraderService.createTrader)
+router.get('/search/:searchString', auth('user', 'admin'),TraderService.searchTrader)
+router.put('/:id', auth('admin'),upload.single("traderPhoto"), TraderService.updateTrader)
+router.get('/copiers', auth('admin'),TraderService.getCopiers)
+router.get('/copiers/:id', auth('admin'),TraderService.declineCopiers)
 
 module.exports = router
