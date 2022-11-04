27521ec3e1fa26ffa448376346b5fa3ad554a83e
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-11-04 21:14:24 +0100

    Api for currency

--- routes/Stock.js
+++ routes/Stock.js
@@ -4,9 +4,9 @@ const Stock = require('../service/Stock')
 
 router.use(protect)
 
-router.post('/currency', auth('user', 'admin'), Stock.getCurrencies)
-router.post('/crypto', auth('user', 'admin') ,Stock.getCrypto)
-router.post('/stock', auth('user', 'admin'), Stock.getStocks)
+router.get('/currency', auth('user', 'admin'), Stock.getCurrencies)
+router.get('/crypto', auth('user', 'admin') ,Stock.getCrypto)
+router.get('/stock', auth('user', 'admin'), Stock.getStocks)
 
 router.post('/open-trade', auth('admin') ,Stock.openTrade)
 router.post('/close-trade', auth('admin'), Stock.closeTrade)
