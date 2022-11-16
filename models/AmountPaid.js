6262e63ee204d570f938adef5b8b459f303b5c08
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-11-16 04:44:11 +0100

    Added a default property to the amountPaid model

--- models/AmountPaid.js
+++ models/AmountPaid.js
@@ -2,7 +2,7 @@ const {model, Schema} = require('mongoose')
 
 const AmountPaid = new Schema ({
     amount:{type:Array},
-    balance:Number,
+    balance:{type:Number, default:0},
     user:{ type: Schema.Types.ObjectId, ref:"User" } 
 })
 
