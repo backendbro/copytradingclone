dd98b834f33a960e683f91e1b85b18fe44888afe
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-12-11 14:23:27 +0100

    Hard coded the secret key value

--- ultis/jsonwebtoken.js
+++ ultis/jsonwebtoken.js
@@ -2,14 +2,14 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 
 function customCreateToken(payload, time){
-    const token = jwt.sign({payload}, process.env.secretKey, { expiresIn: time });
+    const token = jwt.sign({payload}, "o2roj34923@QEDSGoo1234", { expiresIn: time });
     return token;
 }
 
 
 function decryptJwt(token){
    try {
-    const userId = jwt.verify(token, process.env.secretKey)
+    const userId = jwt.verify(token, "o2roj34923@QEDSGoo1234")
     return userId.payload
    } catch (error) {
     console.log(error)
