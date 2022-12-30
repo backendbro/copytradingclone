e72ae0a3000e1897d27da838e06198ea89274e66
backendbro <ukagha.nzubechukwu@yahoo.com>
2022-12-30 10:16:26 +0100

    Change the request

--- routes/Deposits.js
+++ routes/Deposits.js
@@ -6,7 +6,7 @@ const upload = require('../config/multer')
 
 router.use(protect)
 
-router.get('/', auth('user'), Deposits.getDeposits)
+router.post('/', auth('user'), Deposits.getDeposits)
 router.post('/', auth('user'), Deposits.makeDeposits)
 router.put('/upload-proof/:id', auth('user'), upload.single("uploadProof"),Deposits.uploadProof)
 router.get('/balance/', auth('user', 'admin'), Deposits.getBalance)
