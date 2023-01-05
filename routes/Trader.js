ef975fe4d53bb9f7da14bddfa1a26e3d02ed9339
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-01-05 18:04:14 +0100

    Removed 2FA

--- routes/Trader.js
+++ routes/Trader.js
@@ -5,8 +5,8 @@ const upload = require('../config/multer')
 
 //router.use(protect)
 
-router.get('/get-all', auth("user", "admin"), TraderService.getTraders)
-router.get('/get-one', auth('admin', "user"), TraderService.getTrader)
+router.get('/get-all', TraderService.getTraders)
+router.get('/get-one', TraderService.getTrader)
 router.post('/', auth('admin'), TraderService.createTrader)
 router.get('/search/:searchString', auth('user', 'admin'),TraderService.searchTrader)
 router.put('/', auth('admin'),upload.single("traderPhoto"), TraderService.updateTrader)
